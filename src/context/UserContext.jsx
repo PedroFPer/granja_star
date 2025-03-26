866495954043e29afa74ad87010a0d76fe98b705
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-03-26 16:28:08 -0300

    Adicionada configuração inicial do react-route e criado arquivo de contexto

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "./firebase"; 
+
+const UserContext = createContext();
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
