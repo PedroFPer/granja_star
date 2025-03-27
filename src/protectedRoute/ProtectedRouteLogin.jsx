b10784547c5fe7372ea28284d24e5a8b23ffd8c3
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-03-27 15:47:53 -0300

    Realização de ajustes na persistencia de dados

--- src/protectedRoute/ProtectedRouteLogin.jsx
+++ src/protectedRoute/ProtectedRouteLogin.jsx
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react";
 import { useUser } from "../context/UserContext.jsx";
 import { Navigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function ProtectedRouteLogin({ children }) {
-  const { user } = useUser();
-  const [loading, setLoading] = useState(true); 
+  const { user, setUser } = useUser();
+  const [loading, setLoading] = useState(true); // Começa com loading como true
 
   useEffect(() => {
+    // Verificar estado de autenticação com Firebase
+    const auth = getAuth();
 
-    const checkUser = async () => {
-      if (user !== undefined) {
-        setLoading(false);
+    // Função que será chamada quando o estado de autenticação mudar
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) {
+        // Se o usuário estiver autenticado, armazene no contexto
+        setUser(firebaseUser);
+      } else {
+        // Caso contrário, defina o usuário como null
+        setUser(null);
       }
-    };
-    checkUser();
-  }, [user]); 
+      setLoading(false); // Depois que a verificação estiver completa, desativa o loading
+    });
+
+    return () => unsubscribe(); // Limpa o listener quando o componente for desmontado
+  }, [setUser]); // Re-executa o useEffect se o setUser mudar
 
   if (loading) {
-    return null; 
+    return <div>Carregando...</div>; // Exibe "Carregando..." durante o loading
   }
 
-  return user ? <Navigate to="/granja_star/dados" /> : children;
+  if (user) {
+    // Se o usuário estiver logado, redireciona para a página de dados
+    return <Navigate to="/granja_star/dados" />;
+  }
+
+  // Se não estiver logado, renderiza a tela de login
+  return children;
 }
 
 export default ProtectedRouteLogin;
