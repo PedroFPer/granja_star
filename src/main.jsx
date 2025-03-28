b79cc7c06731d68f4b71bff7115dcd06604dce7a
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-03-28 07:26:42 -0300

    Tentativa de resolução de problematica na navegação entre componentes no git hub pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { UserProvider } from "./context/UserContext.jsx";
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import DadosGranja from './pages/DadosGranja.jsx'
+import { createHashRouter, RouterProvider, Navigate } from "react-router-dom"; 
+import "./index.css";
+import App from "./App.jsx";
+import DadosGranja from "./pages/DadosGranja.jsx";
 import ProtectedRouteDados from "./protectedRoute/ProtectedRouteDados.jsx";
 import ProtectedRouteLogin from "./protectedRoute/ProtectedRouteLogin.jsx";
 
-
-const router = createBrowserRouter([
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Navigate to="/granja_star" />, 
+  },
   {
     path: "/granja_star",
     element: (
       <ProtectedRouteLogin>
         <App />
-      </ProtectedRouteLogin>),
+      </ProtectedRouteLogin>
+    ),
   },
   {
     path: "/granja_star/dados",
@@ -27,10 +31,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </UserProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
